2030576a4a171a2eae902ddf8965014ffee79b68
Javi <javgon938@alu.centroafuera.es>
2021-09-16 13:21:07 +0200

    Avanzado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 fun main(){
- println("Mi proyecto en Kotlin")
+ var a = "A" //Kotlin da tipo automático
+ val b = "Buenas noches"
+ val nt:String = "3"
+ var error:String? = "Hola"
+ //Para poder tomar null seria var nt: String? = "A"
+println("La variable a es una $a y la constante b es una $b")
+tocahuevos(error)
+println("${System.identityHashCode(error)} es la referencia en memoria de $error")
+}
 
-
+fun tocahuevos(s : String?){
+ println("La longitud de $s es ${s!!.length}")
 }
